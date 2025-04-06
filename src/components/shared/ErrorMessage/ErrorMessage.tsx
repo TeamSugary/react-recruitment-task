4a6874be35c2cfcf17c8304d8e5bd1132dc279e6
nafistarik <nafistarik789@gmail.com>
2025-04-07 05:10:39 +0600

    error message is done with seperate component

--- src/components/shared/ErrorMessage/ErrorMessage.tsx
+++ src/components/shared/ErrorMessage/ErrorMessage.tsx
@@ -9,7 +9,7 @@ const ErrorMessage: React.FC<ErrorMessageProps> = ({ message }) => {
   return (
     <div className="error-message-container">
       <div className="error-icon">
-        <svg className="animated-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <circle cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="2" />
           <path d="M12 8V12" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
           <circle cx="12" cy="16" r="1" fill="currentColor" />
