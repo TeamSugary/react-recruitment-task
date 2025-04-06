ec17fe84552f64d20f245272b278ff25ac8c6e1d
mahfuzHasan2003 <mahfuzhasan2003@yahoo.com>
2025-04-07 02:41:06 +0600

    added complaint card hover effect

--- src/components/complaint-card.tsx
+++ src/components/complaint-card.tsx
@@ -1,7 +1,7 @@
 import { addHours, format, parseISO } from "date-fns"
 import { JSX } from "react"
-import "../styles/complaint-card.css"
 import { Clock } from "lucide-react"
+import "../styles/complaint-card.css"
 
 interface Complaint {
    Id: number
@@ -16,9 +16,9 @@ interface ComplaintCardProps {
 
 export default function ComplaintCard({ complaint }: ComplaintCardProps): JSX.Element {
    return (
-      <div className="card">
-         <h3 className="card-title">{complaint.Title}</h3>
-         <p className="card-body">{complaint.Body}</p>
+      <div className="card complaint-card">
+         <h3 className="card-title">{complaint.Title || 'no data added by user'}</h3>
+         <p className="card-body">{complaint.Body || 'no data added by user'}</p>
          <p className="card-date">
             <Clock size={15} />
             {format(addHours(parseISO(complaint.CreatedAt), 6), "hh:mm a | dd MMM yyyy")}
