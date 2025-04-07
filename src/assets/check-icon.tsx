d3b7a25b6ccc67520924d58c5626b43d758529a9
Nur Alam <nuralam.rsc@gmail.com>
2025-04-07 17:37:39 +0600

    fix: fix all the requirements

---
+++ src/assets/check-icon.tsx
@@ -0,0 +1,21 @@
+import { SVGProps } from "react";
+
+const CheckIcon = ({ className }: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+    >
+      <path d="M18 6 7 17l-5-5" />
+      <path d="m22 10-7.5 7.5L13 16" />
+    </svg>
+  );
+};
+
+export default CheckIcon;
